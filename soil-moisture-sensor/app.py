d8e7f365dc912353502d7c92143fcc6497744b18
Tyeece Simpson <tyreece.simpson@fdbhealth.com>
2021-09-16 11:28:38 +0100

    fixed linting

--- soil-moisture-sensor/app.py
+++ soil-moisture-sensor/app.py
@@ -1,41 +1,45 @@
-from counterfit_connection import CounterFitConnection
-CounterFitConnection.init('127.0.0.1', 5000)
-
 import time
+import json
+from counterfit_connection import CounterFitConnection
 from counterfit_shims_grove.adc import ADC
 from counterfit_shims_grove.grove_relay import GroveRelay
-import json
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 
-connection_string = 'HostName=tyreeceiothub.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=vI8+ER648WQfM6D0jY813cZ9l2IxjBzDtbsq4opW7wk='
 
-adc = ADC()
-relay = GroveRelay(5)
+def main():
+    CounterFitConnection.init('127.0.0.1', 5000)
 
-device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+    connection_string = 'HostName=tyreeceiothub.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=vI8+ER648WQfM6D0jY813cZ9l2IxjBzDtbsq4opW7wk='
+    adc = ADC()
+    relay = GroveRelay(5)
+    device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
 
-print('Connecting')
-device_client.connect()
-print('Connected')
+    print('Connecting')
+    device_client.connect()
+    print('Connected')
 
-def handle_method_request(request):
-    print("Direct method received - ", request.name)
-    
-    if request.name == "relay_on":
-        relay.on()
-    elif request.name == "relay_off":
-        relay.off()
+    def handle_method_request(request):
+        print("Direct method received - ", request.name)
 
-    method_response = MethodResponse.create_from_method_request(request, 200)
-    device_client.send_method_response(method_response)
+        if request.name == "relay_on":
+            relay.on()
+        elif request.name == "relay_off":
+            relay.off()
 
-device_client.on_method_request_received = handle_method_request
+        method_response = MethodResponse.create_from_method_request(request, 200)
+        device_client.send_method_response(method_response)
 
-while True:
-    soil_moisture = adc.read(0)
-    print("Soil moisture:", soil_moisture)
+    device_client.on_method_request_received = handle_method_request
 
-    message = Message(json.dumps({ 'soil_moisture': soil_moisture }))
-    device_client.send_message(message)
+    while True:
+        soil_moisture = adc.read(0)
+        print("Soil moisture:", soil_moisture)
 
-    time.sleep(10)
+        message = Message(json.dumps({'soil_moisture': soil_moisture}))
+        device_client.send_message(message)
+
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
