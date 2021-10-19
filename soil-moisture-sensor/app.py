6ef9648ff94f59060d902a9bfd6faf447c1a2c85
Tyeece Simpson <tyreece.simpson@fdbhealth.com>
2021-10-19 11:53:27 +0100

    Added unit tests

--- soil-moisture-sensor/app.py
+++ soil-moisture-sensor/app.py
@@ -1,45 +1,51 @@
-import time
-import json
+from azure.iot import device
 from counterfit_connection import CounterFitConnection
+
+import time
 from counterfit_shims_grove.adc import ADC
 from counterfit_shims_grove.grove_relay import GroveRelay
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 
+import sys
 
+adc = ADC()
+relay = GroveRelay(5)
+connection_string = 'HostName=tyreeceiothub.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=vI8+ER648WQfM6D0jY813cZ9l2IxjBzDtbsq4opW7wk='
+device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
 def main():
     CounterFitConnection.init('127.0.0.1', 5000)
-
-    connection_string = 'HostName=tyreeceiothub.azure-devices.net;DeviceId=soil-moisture-sensor;SharedAccessKey=vI8+ER648WQfM6D0jY813cZ9l2IxjBzDtbsq4opW7wk='
-    adc = ADC()
-    relay = GroveRelay(5)
-    device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
-
     print('Connecting')
     device_client.connect()
     print('Connected')
+    device_client.on_method_request_received = handle_method_request
+    if "pytest" not in sys.modules:
+        main_loop(device_client)
+    
 
-    def handle_method_request(request):
-        print("Direct method received - ", request.name)
+def handle_method_request(request):
+    print("Direct method received - ", request.name)
+    if request.name == "relay_on":
+        relay.on()
+    elif request.name == "relay_off":
+        relay.off()
 
         if request.name == "relay_on":
             relay.on()
         elif request.name == "relay_off":
             relay.off()
 
-        method_response = MethodResponse.create_from_method_request(request, 200)
-        device_client.send_method_response(method_response)
 
-    device_client.on_method_request_received = handle_method_request
-
-    while True:
+def main_loop(device_client):
+    while device_client.connected:
         soil_moisture = adc.read(0)
         print("Soil moisture:", soil_moisture)
 
-        message = Message(json.dumps({'soil_moisture': soil_moisture}))
+        message = Message(json.dumps({ 'soil_moisture': soil_moisture }))
+        print(message)
         device_client.send_message(message)
 
         time.sleep(10)
 
 
-if __name__ == "__main__":
+if "pytest" not in sys.modules:
     main()
